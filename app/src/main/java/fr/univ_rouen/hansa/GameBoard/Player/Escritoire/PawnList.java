93cb23cefd34f659fe6fc47abce83bc05a53225f
LeGrandVivien <legrandviv@gmail.com>
2015-01-20 23:21:26 +0100

    feature(player): add implementation of escritoire

---
+++ app/src/main/java/fr/univ_rouen/hansa/GameBoard/Player/Escritoire/PawnList.java
@@ -0,0 +1,27 @@
+package fr.univ_rouen.hansa.GameBoard.Player.Escritoire;
+
+import java.util.List;
+
+import fr.univ_rouen.hansa.GameBoard.Pawns.Merchant;
+import fr.univ_rouen.hansa.GameBoard.Pawns.Pawn;
+import fr.univ_rouen.hansa.GameBoard.Pawns.Trader;
+
+public class PawnList implements IPawnList {
+    @Override
+    public void addPawns(List<Pawn> pawns) {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<Merchant> getMerchants(int merchants) throws IllegalStateException {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<Trader> getTraders(int traders) throws IllegalStateException {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+}
