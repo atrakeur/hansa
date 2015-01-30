a11a9d5e23a6a84fbcddb556305f14b3c05c0645
vivienlegrand <legrandviv@gmail.com>
2015-01-30 23:30:31 +0100

    feature (Plateau): add HTPlayer correct implementation

--- app/src/main/java/fr/univ_rouen/hansa/gameboard/player/HTPlayer.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/player/HTPlayer.java
@@ -1,28 +1,25 @@
 package fr.univ_rouen.hansa.gameboard.player;
 
+import fr.univ_rouen.hansa.gameboard.player.escritoire.Escritoire;
 import fr.univ_rouen.hansa.gameboard.player.escritoire.IEscritoire;
 
 public class HTPlayer implements IHTPlayer {
     private final PlayerColor color;
+    private final IEscritoire escritoire;
 
     public HTPlayer(PlayerColor color, int startingPlace) {
         this.color = color;
+
+        this.escritoire = new Escritoire(startingPlace);
     }
 
     @Override
     public IEscritoire getEscritoire() {
-        //TODO
-        throw new UnsupportedOperationException();
+        return escritoire;
     }
 
     @Override
     public PlayerColor getPlayerColor() {
         return color;
-    }
-
-    @Override
-    public int getCount() {
-        //TODO
-        throw new UnsupportedOperationException();
     }
 }
--- app/src/main/java/fr/univ_rouen/hansa/gameboard/player/IHTPlayer.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/player/IHTPlayer.java
@@ -3,9 +3,18 @@ package fr.univ_rouen.hansa.gameboard.player;
 import fr.univ_rouen.hansa.gameboard.player.escritoire.IEscritoire;
 
 public interface IHTPlayer {
+
+    /**
+     * Access to the escritoire and all the informations of the player
+     *
+     * @return the escritoire
+     */
     public IEscritoire getEscritoire();
 
+    /**
+     * Return the color of the player, equivalent of his ID
+     *
+     * @return PlayerColor, an enum who represent the color
+     */
     public PlayerColor getPlayerColor();
-
-    public int getCount();
 }
