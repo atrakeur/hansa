1c44a1e718501bab6afcc1af52a4c559bf67b6ab
vivienlegrand <legrandviv@gmail.com>
2015-01-30 15:32:43 +0100

    feature (Plateau): Add implementation of turnManager

--- app/src/main/java/fr/univ_rouen/hansa/gameboard/TurnManager.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/TurnManager.java
@@ -1,19 +1,48 @@
 package fr.univ_rouen.hansa.gameboard;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+import fr.univ_rouen.hansa.gameboard.player.HTPlayer;
 import fr.univ_rouen.hansa.gameboard.player.IHTPlayer;
+import fr.univ_rouen.hansa.gameboard.player.PlayerColor;
 
 public class TurnManager {
     private static TurnManager ourInstance = new TurnManager();
+
+    private final List<IHTPlayer> players;
+    private int position;
 
     public static TurnManager getInstance() {
         return ourInstance;
     }
 
     private TurnManager() {
+        players = Lists.newArrayList();
+    }
+
+    public void addPlayers(List<PlayerColor> playersColors) {
+        if (players.size() > 0) {
+            players.clear();
+        }
+
+        position = 0;
+
+        int i = 0;
+
+        for (PlayerColor color : playersColors) {
+            players.add(new HTPlayer(color, ++i));
+        }
     }
 
     public IHTPlayer getCurrentPlayer() {
-        throw new UnsupportedOperationException();
-        //TODO implementation
+        return players.get(position);
+    }
+
+    public void nextPlayer() {
+        if (++position >= players.size()) {
+            position = 0;
+        }
     }
 }
