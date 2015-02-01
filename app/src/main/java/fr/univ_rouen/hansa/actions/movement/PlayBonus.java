0dadd3f49a1c4db298b171a13040ba11b5b37bfe
Valulz <vdesportes@gmail.com>
2015-02-01 22:16:49 +0100

    feat(Plateau) : Add PlayBonus Action

--- app/src/main/java/fr/univ_rouen/hansa/actions/movement/PlayBonus.java
+++ app/src/main/java/fr/univ_rouen/hansa/actions/movement/PlayBonus.java
@@ -1,25 +1,49 @@
 package fr.univ_rouen.hansa.actions.movement;
 
 import fr.univ_rouen.hansa.actions.Actions;
+import fr.univ_rouen.hansa.gameboard.bonusmarkers.BonusState;
+import fr.univ_rouen.hansa.gameboard.bonusmarkers.IBonusMarker;
 
 public class PlayBonus implements IMovement {
+
+    private IBonusMarker bonusMarker;
+    private boolean actionDone;
+
+    public PlayBonus(IBonusMarker bonus){
+
+        if(bonus == null || bonus.getState() != BonusState.onHand){
+            throw new IllegalArgumentException();
+        }
+
+        this.bonusMarker = bonus;
+        this.actionDone  = false;
+    }
+
     @Override
     public boolean isDone() {
-        return false;
+        return actionDone;
     }
 
     @Override
     public Actions getActionDone() {
-        return null;
+        return Actions.playBonus;
     }
 
     @Override
     public void doMovement() {
+        if(this.isDone() || this.bonusMarker.getState() != BonusState.onHand){
+            throw new IllegalStateException();
+        }
 
+        bonusMarker.doAction();
     }
 
     @Override
     public void doRollback() {
+        if(!this.isDone() || this.bonusMarker.getState() == BonusState.onHand){
+            throw new IllegalStateException();
+        }
 
+        //??? euh undoAction ?
     }
 }
