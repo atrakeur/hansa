2a5b8b621e7b7c34be1c1b5feb6e288471415bcd
Aku76 <steeven.cauchois@gmail.com>
2015-02-04 18:18:37 +0100

    fix <bonusMarker>:   add setState for IBonusMarker ( & commentary )   add BonusState pickedThisTurn & inPlate ( & commentary )

--- app/src/main/java/fr/univ_rouen/hansa/gameboard/bonusmarkers/IBonusMarker.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/bonusmarkers/IBonusMarker.java
@@ -1,6 +1,13 @@
 package fr.univ_rouen.hansa.gameboard.bonusmarkers;
 
+/**
+ * Represent a Bonus Pawn.
+ *
+ * A Bonus Pawn can be used at the player's turn, once picked up, except the turn which he picked up
+ *   the bonus Pawn
+ */
 public interface IBonusMarker {
     public BonusState getState();
+    public void setState(BonusState state);
     public void doAction();
 }
