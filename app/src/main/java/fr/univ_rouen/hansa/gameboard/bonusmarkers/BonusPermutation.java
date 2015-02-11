a0ab28e07482041ba72d58da65ea66ad19e65101
Aku76 <steeven.cauchois@gmail.com>
2015-02-11 04:12:09 +0100

    fix (BonusPermutation) : adapt to extended StatedBonus version

--- app/src/main/java/fr/univ_rouen/hansa/gameboard/bonusmarkers/BonusPermutation.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/bonusmarkers/BonusPermutation.java
@@ -4,32 +4,20 @@ package fr.univ_rouen.hansa.gameboard.bonusmarkers;
 /**
  * Bonus which allow you to permute two Kontor in a City
  */
-public class BonusPermutation implements IBonusMarker {
-
-    private BonusState state;
+public class BonusPermutation extends StatedBonus implements IBonusMarker {
 
     public BonusPermutation() {
         super();
-        this.state = BonusState.unused;
-    }
-    @Override
-    public BonusState getState() {
-        return state;
-    }
-    @Override
-    public void setState(BonusState state) {
-        if (state == null){
-            throw new NullPointerException();
-        }
-        this.state = state;
     }
 
     @Override
     public void doAction() {
+        super.doAction();
         //TODO
-
     }
+    @Override
     public void undoAction() {
-
+        super.undoAction();
+        //TODO
     }
 }
