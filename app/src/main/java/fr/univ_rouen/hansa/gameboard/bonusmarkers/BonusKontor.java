e43532450187156b7723487bdf5fa10b3340242f
Aku76 <steeven.cauchois@gmail.com>
2015-02-11 03:59:16 +0100

    feature (BonusKontor) : add BonusKontor Class

--- app/src/main/java/fr/univ_rouen/hansa/gameboard/bonusmarkers/BonusKontor.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/bonusmarkers/BonusKontor.java
@@ -1,40 +1,85 @@
 package fr.univ_rouen.hansa.gameboard.bonusmarkers;
 
+import fr.univ_rouen.hansa.gameboard.Privillegium;
 import fr.univ_rouen.hansa.gameboard.TurnManager;
+import fr.univ_rouen.hansa.gameboard.cities.ICity;
+import fr.univ_rouen.hansa.gameboard.cities.IKontor;
 import fr.univ_rouen.hansa.gameboard.cities.Kontor;
-import fr.univ_rouen.hansa.gameboard.player.escritoire.IEscritoire;
+import fr.univ_rouen.hansa.gameboard.pawns.Pawn;
+import fr.univ_rouen.hansa.gameboard.player.IHTPlayer;
 
 
 /**
  * Bonus which allow you to add a Kontor on the left of a City
  */
-public class BonusKontor implements IBonusMarker {
-
-    private BonusState state;
+public class BonusKontor extends StatedBonus implements IBonusMarker {
+    private ICity city;
+    private Pawn pawn;
 
     public BonusKontor() {
         super();
-        this.state = BonusState.unused;
     }
-    @Override
-    public BonusState getState() {
-        return state;
-    }
-    @Override
-    public void setState(BonusState state) {
-        if (state == null){
-            throw new NullPointerException();
+
+    /**
+     * initialise the city which will have a bonus Kontor
+     */
+    public void setCity(ICity city){
+        if(city == null){
+            throw new IllegalArgumentException();
         }
-        this.state = state;
+        if(city.getKontors().isEmpty()){
+            throw new IllegalStateException("City has to have at least one Kontor filed to use " +
+                    "a BonusKontor on it");
+        }
+        this.city = city;
+    }
+
+    /**
+     * initialise the pawn which will be in the Bonus Kontor
+     */
+    public void setPawn(Pawn pawn) {
+        if(pawn == null) {
+            throw new IllegalArgumentException();
+        }
+        if(!pawn.getPlayer().getEscritoire().getBonusMarker().contains(this)){
+            throw new IllegalStateException("The pawn's player differs of the marker's owner");
+        }
+        this.pawn = pawn;
     }
 
     @Override
     public void doAction() {
-        //TODO
-        IEscritoire escritoire = TurnManager.getInstance().getCurrentPlayer().getEscritoire();
-
+        super.doAction();
+        if(city == null){
+            throw new IllegalStateException("a city must have been set");
+        }
+        if(pawn == null){
+            throw new IllegalStateException("a pawn must have been set");
+        }
+        IHTPlayer player = TurnManager.getInstance().getCurrentPlayer();
+        if(!player.getEscritoire().getBonusMarker().contains(this)){
+            throw new IllegalStateException("The current player differs of the marker's owner");
+        }
+        IKontor<Pawn> k = new Kontor<Pawn>(Privillegium.White);
+        //TODO enlever le pion de la reserve du joueur ? (cf undo)
+        k.pullPawn(pawn);
+        city.pushAdditionalKontors(k);
     }
-    public void undoAction() {
 
+    public void undoAction() {
+        super.undoAction();
+        if(city == null){
+            throw new IllegalStateException("a city must have been set");
+        }
+        if(pawn == null){
+            throw new IllegalStateException("a pawn must have been set");
+        }
+        IHTPlayer player = TurnManager.getInstance().getCurrentPlayer();
+        if(!player.getEscritoire().getBonusMarker().contains(this)){
+            throw new IllegalStateException("The current player differs of the marker's owner");
+        }
+
+        //TODO remettre le pion dans la reserve du joueur ? (cf do)
+        city.getAdditionalKontors().remove(city.getAdditionalKontors().size()-1);
     }
 }
