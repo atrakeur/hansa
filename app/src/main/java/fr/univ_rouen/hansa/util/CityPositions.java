ca4175bc6870852700096e02e8dda5bf4995128e
Sindael117 <houbartjulien@live.fr>
2015-02-13 11:59:46 +0100

    fix (GameBoard): Corrected city positions

--- app/src/main/java/fr/univ_rouen/hansa/util/CityPositions.java
+++ app/src/main/java/fr/univ_rouen/hansa/util/CityPositions.java
@@ -5,32 +5,33 @@ import fr.univ_rouen.hansa.view.Position;
 
 public class CityPositions {
 
-    public static IPosition GRONINGEN= new Position(0.06, 0.174);
-    public static IPosition EMDEN= new Position(0.271, 0.168);
-    public static IPosition BREMEN= new Position(0.49, 0.238);
-    public static IPosition STADE= new Position(0.533, 0.109);
-    public static IPosition HAMBURG= new Position(0.775, 0.076);
-    public static IPosition LUBECK= new Position(0.903, 0.087);
-    public static IPosition KAMPEN= new Position(0.05, 0.337);
-    public static IPosition OSNABRUCK= new Position(0.26, 0.455);
-    public static IPosition HANNOVER= new Position(0.61, 0.424);
-    public static IPosition LUNEBURG= new Position(0.787, 0.312);
-    public static IPosition PERLBERG= new Position(0.94, 0.261);
-    public static IPosition ARNHEIM= new Position(0.066, 0.566);
-    public static IPosition MUNSTER= new Position(0.272, 0.616);
-    public static IPosition MINDEN= new Position(0.465, 0.543);
-    public static IPosition BRUNSWIEK= new Position(0.726, 0.503);
-    public static IPosition STENDAL= new Position(0.932, 0.554);
-    public static IPosition DUISBURG= new Position(0.082, 0.784);
-    public static IPosition DORTMUND= new Position(0.246, 0.778);
-    public static IPosition PADERBORN= new Position(0.457, 0.806);
-    public static IPosition HILDESHEIM= new Position(0.633, 0.653);
-    public static IPosition GOSLAR= new Position(0.799, 0.731);
-    public static IPosition MAGDEBURG= new Position(0.941, 0.819);
-    public static IPosition COELLEN= new Position(0.051, 0.926);
-    public static IPosition MARBURG= new Position(0.36, 0.976); 
-    public static IPosition GOTTINGEN= new Position(0.59, 0.942);
-    public static IPosition QUEDLINBURG= new Position(0.827, 0.864);
-    public static IPosition HALLE= new Position(0.906, 0.931); 
+    public static IPosition GRONINGEN= new Position(0.06f, 0.174f);
+public static IPosition EMDEN= new Position(0.271f, 0.168f);
+public static IPosition BREMEN= new Position(0.49f, 0.238f);
+public static IPosition STADE= new Position(0.533f, 0.109f);
+public static IPosition HAMBURG= new Position(0.775f, 0.076f);
+public static IPosition LUBECK= new Position(0.903f, 0.087f);
+public static IPosition KAMPEN= new Position(0.05f, 0.337f);
+public static IPosition OSNABRUCK= new Position(0.26f, 0.455f);
+public static IPosition HANNOVER= new Position(0.61f, 0.424f);
+public static IPosition LUNEBURG= new Position(0.787f, 0.312f);
+public static IPosition PERLBERG= new Position(0.94f, 0.261f);
+public static IPosition ARNHEIM= new Position(0.066f, 0.566f);
+public static IPosition MUNSTER= new Position(0.272f, 0.616f);
+public static IPosition MINDEN= new Position(0.465f, 0.543f);
+public static IPosition BRUNSWIEK= new Position(0.726f, 0.503f);
+public static IPosition STENDAL= new Position(0.932f, 0.554f);
+public static IPosition DUISBURG= new Position(0.082f, 0.784f);
+public static IPosition DORTMUND= new Position(0.246f, 0.778f);
+public static IPosition PADERBORN= new Position(0.457f, 0.806f);
+public static IPosition HILDESHEIM= new Position(0.633f, 0.653f);
+public static IPosition GOSLAR= new Position(0.799f, 0.731f);
+public static IPosition MAGDEBURG= new Position(0.941f, 0.819f);
+public static IPosition COELLEN= new Position(0.051f, 0.926f);
+public static IPosition MARBURG= new Position(0.36f, 0.976f); 
+public static IPosition GOTTINGEN= new Position(0.59f, 0.942f);
+public static IPosition QUEDLINBURG= new Position(0.827f, 0.864f);
+public static IPosition HALLE= new Position(0.906f, 0.931f); 
+
 
 }
