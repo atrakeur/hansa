b3b9b2084dd0e45ec67b4a288938ce8f3a7a479b
Aku76 <steeven.cauchois@gmail.com>
2015-02-14 15:51:15 +0100

    feature (BonusPermutation) : implementation done

--- app/src/main/java/fr/univ_rouen/hansa/gameboard/bonusmarkers/BonusPermutation.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/bonusmarkers/BonusPermutation.java
@@ -1,10 +1,16 @@
 package fr.univ_rouen.hansa.gameboard.bonusmarkers;
 
 
+import fr.univ_rouen.hansa.gameboard.cities.IKontor;
+import fr.univ_rouen.hansa.gameboard.pawns.Pawn;
+
 /**
  * Bonus which allow you to permute two Kontor in a City
  */
 public class BonusPermutation extends StatedBonus implements IBonusMarker {
+
+    private IKontor<Pawn> k1;
+    private IKontor<Pawn> k2;
 
     public BonusPermutation() {
         super();
@@ -13,11 +19,44 @@ public class BonusPermutation extends StatedBonus implements IBonusMarker {
     @Override
     public void doAction() {
         super.doAction();
-        //TODO
+        if (k1 == null || k2 == null){
+            throw new NullPointerException();
+        }
+        if (k1.isEmpty() || k2.isEmpty()) {
+            throw new IllegalStateException("A kontor is empty");
+        }
+        //TODO check if k1 & k2 are in the same city
+        Pawn p1 = k1.pushPawn();
+        Pawn p2 = k2.pushPawn();
+        k1.pullPawn(p2);
+        k2.pullPawn(p1);
     }
     @Override
     public void undoAction() {
         super.undoAction();
-        //TODO
+        if (k1 == null || k2 == null){
+            throw new NullPointerException();
+        }
+        if (k1.isEmpty() || k2.isEmpty()) {
+            throw new IllegalStateException("A kontor is empty");
+        }
+        //TODO check if k1 & k2 are in the same city
+        Pawn p1 = k1.pushPawn();
+        Pawn p2 = k2.pushPawn();
+        k1.pullPawn(p2);
+        k2.pullPawn(p1);
+    }
+
+    public void setKontor1(IKontor<Pawn> k){
+        if (k == null){
+            throw new NullPointerException();
+        }
+        k1 = k;
+    }
+    public void setKontor2(IKontor<Pawn> k){
+        if (k == null){
+            throw new NullPointerException();
+        }
+        k2 = k;
     }
 }
