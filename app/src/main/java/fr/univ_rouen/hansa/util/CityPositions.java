4317c32837284be2abcd19d12d0b90e2cf04737a
Affluxx <Affluxxx@gmail.com>
2015-02-16 21:54:16 +0100

    <fix> CityPositions: Modify positions

--- app/src/main/java/fr/univ_rouen/hansa/util/CityPositions.java
+++ app/src/main/java/fr/univ_rouen/hansa/util/CityPositions.java
@@ -5,32 +5,59 @@ import fr.univ_rouen.hansa.view.Position;
 
 public class CityPositions {
 
-    public static IPosition GRONINGEN = new Position(0.06f, 0.174f);
-    public static IPosition EMDEN = new Position(0.271f, 0.168f);
-    public static IPosition BREMEN = new Position(0.49f, 0.238f);
-    public static IPosition STADE = new Position(0.533f, 0.109f);
-    public static IPosition HAMBURG = new Position(0.775f, 0.076f);
-    public static IPosition LUBECK = new Position(0.903f, 0.087f);
-    public static IPosition KAMPEN = new Position(0.05f, 0.337f);
-    public static IPosition OSNABRUCK = new Position(0.26f, 0.455f);
-    public static IPosition HANNOVER = new Position(0.61f, 0.424f);
-    public static IPosition LUNEBURG = new Position(0.787f, 0.312f);
-    public static IPosition PERLBERG = new Position(0.94f, 0.261f);
-    public static IPosition ARNHEIM = new Position(0.066f, 0.566f);
-    public static IPosition MUNSTER = new Position(0.272f, 0.616f);
-    public static IPosition MINDEN = new Position(0.465f, 0.543f);
-    public static IPosition BRUNSWIEK = new Position(0.726f, 0.503f);
-    public static IPosition STENDAL = new Position(0.932f, 0.554f);
-    public static IPosition DUISBURG = new Position(0.082f, 0.784f);
-    public static IPosition DORTMUND = new Position(0.246f, 0.778f);
-    public static IPosition PADERBORN = new Position(0.457f, 0.806f);
-    public static IPosition HILDESHEIM = new Position(0.633f, 0.653f);
-    public static IPosition GOSLAR = new Position(0.799f, 0.731f);
-    public static IPosition MAGDEBURG = new Position(0.941f, 0.819f);
-    public static IPosition COELLEN = new Position(0.051f, 0.926f);
-    public static IPosition MARBURG = new Position(0.36f, 0.976f);
-    public static IPosition GOTTINGEN = new Position(0.59f, 0.942f);
-    public static IPosition QUEDLINBURG = new Position(0.827f, 0.864f);
-    public static IPosition HALLE = new Position(0.906f, 0.931f);
+    //ok
+    public static IPosition GRONINGEN = new Position(0.062f, 0.156f);
+    // ok
+    public static IPosition EMDEN = new Position(0.273f, 0.151f);
+    //ok
+    public static IPosition BREMEN = new Position(0.49f, 0.218f);
+    //ok
+    public static IPosition STADE = new Position(0.534f, 0.089f);
+    //ok
+    public static IPosition HAMBURG = new Position(0.757f, 0.057f);
+    //ok
+    public static IPosition LUBECK = new Position(0.904f, 0.068f);
+    //ok
+    public static IPosition KAMPEN = new Position(0.05f, 0.317f);
+    //ok
+    public static IPosition OSNABRUCK = new Position(0.26f, 0.435f);
+    //ok
+    public static IPosition HANNOVER = new Position(0.61f, 0.404f);
+    //
+    public static IPosition LUNEBURG = new Position(0.787f, 0.288f);
+    //
+    public static IPosition PERLBERG = new Position(0.94f, 0.241f);
+    //
+    public static IPosition ARNHEIM = new Position(0.066f, 0.546f);
+    //
+    public static IPosition MUNSTER = new Position(0.272f, 0.596f);
+    //ok
+    public static IPosition MINDEN = new Position(0.465f, 0.523f);
+    //ok
+    public static IPosition BRUNSWIEK = new Position(0.726f, 0.483f);
+    //
+    public static IPosition STENDAL = new Position(0.932f, 0.532f);
+    //ok
+    public static IPosition DUISBURG = new Position(0.082f, 0.764f);
+    // ok, a retest avec une tablette
+    public static IPosition DORTMUND = new Position(0.247f, 0.759f);
+    //ok
+    public static IPosition PADERBORN = new Position(0.460f, 0.786f);
+    //ok
+    public static IPosition HILDESHEIM = new Position(0.635f, 0.633f);
+    //ok
+    public static IPosition GOSLAR = new Position(0.802f, 0.709f);
+    //ok
+    public static IPosition MAGDEBURG = new Position(0.941f, 0.793f);
+    //ok
+    public static IPosition COELLEN = new Position(0.051f, 0.906f);
+    //ok
+    public static IPosition MARBURG = new Position(0.36f, 0.956f);
+    //ok
+    public static IPosition GOTTINGEN = new Position(0.592f, 0.920f);
+    //ok
+    public static IPosition QUEDLINBURG = new Position(0.828f, 0.840f);
+    //ok
+    public static IPosition HALLE = new Position(0.908f, 0.909f);
 
 }
