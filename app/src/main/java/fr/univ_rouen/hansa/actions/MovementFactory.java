9fd769783d1d3543afe2acaf9326cb3618b5ce7d
atrakeur <atrakeur@gmail.com>
2015-02-23 14:48:58 +0100

    fix (UI): fixed crash on invalid actions

---
+++ app/src/main/java/fr/univ_rouen/hansa/actions/MovementFactory.java
@@ -0,0 +1,62 @@
+package fr.univ_rouen.hansa.actions;
+
+import fr.univ_rouen.hansa.actions.movement.Bursa;
+import fr.univ_rouen.hansa.actions.movement.IMovement;
+import fr.univ_rouen.hansa.actions.movement.KeepKontor;
+import fr.univ_rouen.hansa.actions.movement.KeepRoute;
+import fr.univ_rouen.hansa.actions.movement.LiberSophia;
+import fr.univ_rouen.hansa.actions.movement.MovePawnRtoGB;
+import fr.univ_rouen.hansa.exceptions.GameException;
+import fr.univ_rouen.hansa.gameboard.TurnManager;
+import fr.univ_rouen.hansa.gameboard.cities.ICity;
+import fr.univ_rouen.hansa.gameboard.player.IHTPlayer;
+import fr.univ_rouen.hansa.gameboard.player.pawns.Trader;
+import fr.univ_rouen.hansa.gameboard.routes.IVillage;
+import fr.univ_rouen.hansa.view.interactions.IClickableArea;
+
+public class MovementFactory {
+    private static MovementFactory ourInstance = new MovementFactory();
+
+    public static MovementFactory getInstance() {
+        return ourInstance;
+    }
+
+    private MovementFactory() {
+    }
+
+    public IMovement makeMovement(IClickableArea source, IClickableArea destination) {
+        IHTPlayer player = TurnManager.getInstance().getCurrentPlayer();
+
+        if (source.getType() == IClickableArea.Type.bonus && destination == null ) {
+            //TODO PlayBonus Bonus -> null
+            throw new UnsupportedOperationException();
+        } else if (source.getType() == IClickableArea.Type.village && destination == null ) {
+            return new KeepRoute(player, ((IVillage) source.getSubject()).getRoute());
+        } else if (source.getType() == IClickableArea.Type.village && destination.getType() == IClickableArea.Type.village ) {
+            //TODO Check avec les differents players
+            return new LiberSophia(player, (IVillage) source.getSubject(), (IVillage) destination.getSubject());
+        } else if (source.getType() == IClickableArea.Type.village && destination.getType() == IClickableArea.Type.power ) {
+            //TODO IncreasePower Village -> Power
+            throw new UnsupportedOperationException();
+        } else if (source.getType() == IClickableArea.Type.supply && destination.getType() == IClickableArea.Type.village ) {
+            //TODO enlever trader en dur
+            return new MovePawnRtoGB(player, (IVillage) destination.getSubject(), Trader.class);
+        } else if (source.getType() == IClickableArea.Type.village && destination.getType() == IClickableArea.Type.city ) {
+            return new KeepKontor(player, (ICity) destination.getSubject(), (IVillage) source.getSubject());
+        }
+
+        throw new GameException("Invalid movement");
+    }
+
+    public IMovement makeBursaMovement(int merchant) {
+        IHTPlayer player = TurnManager.getInstance().getCurrentPlayer();
+
+        return new Bursa(player, merchant);
+    }
+
+    public IMovement makeBursaMovement() {
+        IHTPlayer player = TurnManager.getInstance().getCurrentPlayer();
+
+        return new Bursa(player);
+    }
+}
