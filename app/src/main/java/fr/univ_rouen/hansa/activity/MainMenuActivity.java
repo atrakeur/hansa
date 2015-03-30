7b51c876732621c33c8b932b8f3b6dc067b41970
Valulz <vdesportes@gmail.com>
2015-03-30 21:50:14 +0200

    update(UI) : Add Option Button to Main Menu

--- app/src/main/java/fr/univ_rouen/hansa/activity/MainMenuActivity.java
+++ app/src/main/java/fr/univ_rouen/hansa/activity/MainMenuActivity.java
@@ -2,7 +2,6 @@ package fr.univ_rouen.hansa.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 
 import fr.univ_rouen.hansa.R;
@@ -15,27 +14,16 @@ public class MainMenuActivity extends Activity {
         setContentView(R.layout.activity_main_menu);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void newGame(View view){
+    public void newGame(View view) {
         //TODO appeller une nouvelle activité NewGameActivity
     }
 
-    public void loadGame(View view){
+    public void loadGame(View view) {
         //TODO appeller une nouvelle activité LoadGameActivity ou une popup  ???
     }
 
+    public void options(View view) {
+        //TODO appeller une nouvelle activité OptionsActivity
+
+    }
 }
