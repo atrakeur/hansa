87878b50cce7397011e27f04ceb57734b4062fd0
Valulz <vdesportes@gmail.com>
2015-04-08 00:02:58 +0200

    refactor(UI) : Clean up GameActivity, CustomAdapter, their respectives xml Put all elements from GameActivity in relation with new game into NewGameActivity NewGameActivity is the launching Activity

--- app/src/main/java/fr/univ_rouen/hansa/activity/GameActivity.java
+++ app/src/main/java/fr/univ_rouen/hansa/activity/GameActivity.java
@@ -4,20 +4,13 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,30 +18,23 @@ import fr.univ_rouen.hansa.R;
 import fr.univ_rouen.hansa.actions.MovementFactory;
 import fr.univ_rouen.hansa.actions.MovementManager;
 import fr.univ_rouen.hansa.actions.movement.IMovement;
-import fr.univ_rouen.hansa.adapter.CustomAdapter;
 import fr.univ_rouen.hansa.gameboard.TurnManager;
 import fr.univ_rouen.hansa.gameboard.player.IHTPlayer;
 import fr.univ_rouen.hansa.gameboard.player.PlayerColor;
 import fr.univ_rouen.hansa.gameboard.player.pawns.Merchant;
 import fr.univ_rouen.hansa.gameboard.player.pawns.Pawn;
 import fr.univ_rouen.hansa.view.interactions.AlertDialogBursa;
-import fr.univ_rouen.hansa.view.utils.ListModel;
 
 public class GameActivity extends Activity {
 
     private Context context = this;
-    ListView list;
-    CustomAdapter adapter;
-    public  GameActivity CustomListView = null;
-    public ArrayList<ListModel> CustomListViewValuesArr = new ArrayList<ListModel>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         TurnManager.getInstance().addPlayers(Arrays.asList(PlayerColor.values()));
-        IHTPlayer player =TurnManager.getInstance().getCurrentPlayer();
+        IHTPlayer player = TurnManager.getInstance().getCurrentPlayer();
 
 
         //TODO just pour la présentation, à enlever après ;)
@@ -61,75 +47,13 @@ public class GameActivity extends Activity {
         pawns.add(new Merchant(player));
 
         player.getEscritoire().addToStock(pawns);
-        CustomListView = this;
-
-        /******** Take some data in Arraylist ( CustomListViewValuesArr ) ***********/
-        setListData();
-
-        Resources res =getResources();
-        list = (ListView)findViewById(R.id.list);
-
-        /**************** Create Custom Adapter *********/
-        adapter = new CustomAdapter(CustomListView, CustomListViewValuesArr,res);
-        list.setAdapter(adapter);
-        /**
-         * Le spinner est a nul et ça pose probleme.
-        Spinner spinner = (Spinner) findViewById(R.id.player);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-        this.setSpinnerListener(spinner);
-        */
     }
-
-    /****** Function to set data in ArrayList *************/
-    public void setListData() {
-
-        for (int i = 0; i < 5; i++) {
-
-            final ListModel sched = new ListModel();
-
-            /******* Firstly take data in model object ******/
-            sched.setColor("Red");
-            sched.setPlayer("Player");
-
-            /******** Take Model Object in ArrayList **********/
-            CustomListViewValuesArr.add(sched);
-        }
-    }
-
-    /**
-     * Methode de creation des listener a l'exterieur. Possible de le stocker dans une classe
-     * @param spinner
-     */
-    public void setSpinnerListener(Spinner spinner) {
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Spinner p1 = (Spinner) findViewById(R.id.player);
-                Spinner color1 = (Spinner) findViewById(R.id.color);
-                p1.setOnItemSelectedListener(this);
-                color1.setOnItemSelectedListener(this);
-                String s1 = (String) p1.getSelectedItem();
-                String s2 = (String) color1.getSelectedItem();
-                Log.w("Joueur 1 : ", s1);
-                Log.w("Couleur 1 :" , s2);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-    }
-
-
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        IHTPlayer player =TurnManager.getInstance().getCurrentPlayer();
+        IHTPlayer player = TurnManager.getInstance().getCurrentPlayer();
 
         findViewById(R.id.button_cancel).setBackgroundColor(player.getPlayerColor().getColor());
         findViewById(R.id.button_bursa).setBackgroundColor(player.getPlayerColor().getColor());
@@ -138,35 +62,35 @@ public class GameActivity extends Activity {
         findViewById(R.id.button_pause).setBackgroundColor(player.getPlayerColor().getColor());
     }
 
-    public void toasty(View v){
+    public void toasty(View v) {
         Toast.makeText(getApplicationContext(), "Toasty", Toast.LENGTH_SHORT).show();
     }
 
-    public void bursaAction(View v){
+    public void bursaAction(View v) {
         final IHTPlayer player = TurnManager.getInstance().getCurrentPlayer();
 
-        if(player.getEscritoire().getStock().getMerchantCount() == 0 &&
-                player.getEscritoire().getStock().getTraderCount() == 0){
+        if (player.getEscritoire().getStock().getMerchantCount() == 0 &&
+                player.getEscritoire().getStock().getTraderCount() == 0) {
             Toast.makeText(context, "Action Impossible : Pas de pions à déplacer.", Toast.LENGTH_SHORT).show();
             return;
         }
 
 
-        if(player.getEscritoire().getStock().getMerchantCount() <= 0){
+        if (player.getEscritoire().getStock().getMerchantCount() <= 0) {
             IMovement m = MovementFactory.getInstance().makeBursaMovement(0);
             MovementManager.getInstance().doMove(m);
-            Toast.makeText(context, "Nombre de trader : "+TurnManager.getInstance().getCurrentPlayer().getEscritoire().getSupply().getTraderCount(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Nombre de trader : " + TurnManager.getInstance().getCurrentPlayer().getEscritoire().getSupply().getTraderCount(), Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if(player.getEscritoire().bursaLevel() == Integer.MAX_VALUE){
+        if (player.getEscritoire().bursaLevel() == Integer.MAX_VALUE) {
             IMovement m = MovementFactory.getInstance().makeBursaMovement();
             MovementManager.getInstance().doMove(m);
-            Toast.makeText(context, "Nombre de trader : "+TurnManager.getInstance().getCurrentPlayer().getEscritoire().getSupply().getTraderCount(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Nombre de trader : " + TurnManager.getInstance().getCurrentPlayer().getEscritoire().getSupply().getTraderCount(), Toast.LENGTH_SHORT).show();
             return;
         }
 
-        AlertDialogBursa dialog = new AlertDialogBursa(context, getLayoutInflater(), player );
+        AlertDialogBursa dialog = new AlertDialogBursa(context, getLayoutInflater(), player);
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
 
@@ -176,26 +100,26 @@ public class GameActivity extends Activity {
         final TextView tex = dialog.getResult();
         // set dialog message
         alertDialogBuilder
-            .setView(dialog.getView())
-            .setCancelable(false)
-            .setPositiveButton(R.string.alert_confirm, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    //Generate mouvement and do it!
-                    int merchants = Integer.parseInt( tex.getText()+"" );
-                    IMovement m = MovementFactory.getInstance().makeBursaMovement(merchants);
-                    MovementManager.getInstance().doMove(m);
-                    Toast.makeText(context, "Vous avez "+player.getEscritoire().getSupply().getTraderCount()+" Traders, et "
-                            +player.getEscritoire().getSupply().getMerchantCount()+" Marchants", Toast.LENGTH_SHORT).show();
-                    dialog.dismiss();
-                }
-            })
-            .setNegativeButton(R.string.alert_cancel, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int id) {
-                    // if this button is clicked, just close
-                    // the dialog box and do nothing
-                    dialog.cancel();
-                }
-            });
+                .setView(dialog.getView())
+                .setCancelable(false)
+                .setPositiveButton(R.string.alert_confirm, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //Generate mouvement and do it!
+                        int merchants = Integer.parseInt(tex.getText() + "");
+                        IMovement m = MovementFactory.getInstance().makeBursaMovement(merchants);
+                        MovementManager.getInstance().doMove(m);
+                        Toast.makeText(context, "Vous avez " + player.getEscritoire().getSupply().getTraderCount() + " Traders, et "
+                                + player.getEscritoire().getSupply().getMerchantCount() + " Marchants", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton(R.string.alert_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
 
         // create alert dialog
         AlertDialog alertDialog = alertDialogBuilder.create();
@@ -205,18 +129,14 @@ public class GameActivity extends Activity {
 
     }
 
-    public void pauseAction(View view){
-        if(!MovementManager.getInstance().isEmpty()){
+    public void pauseAction(View view) {
+        if (!MovementManager.getInstance().isEmpty()) {
             MovementManager.getInstance().rollbackMove();
         }
     }
 
-    public void submitAction(View v){
+    public void submitAction(View v) {
         TurnManager.getInstance().nextPlayer(false);
         this.onResume();
-    }
-
-    public void onItemClick(int mPosition) {
-
     }
 }
--- app/src/main/java/fr/univ_rouen/hansa/adapter/CustomAdapter.java
+++ app/src/main/java/fr/univ_rouen/hansa/adapter/CustomAdapter.java
@@ -1,102 +1,100 @@
 package fr.univ_rouen.hansa.adapter;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import fr.univ_rouen.hansa.R;
-import fr.univ_rouen.hansa.activity.GameActivity;
-import fr.univ_rouen.hansa.view.utils.ListModel;
 
-/********* Adapter class extends with BaseAdapter and implements with OnClickListener ************/
-public class CustomAdapter extends BaseAdapter   implements OnClickListener {
+/**
+ * ****** Adapter class extends with BaseAdapter and implements with OnClickListener ***********
+ */
+public class CustomAdapter extends BaseAdapter implements OnClickListener {
 
-    /*********** Declare Used Variables *********/
+    /**
+     * ******** Declare Used Variables ********
+     */
     private Activity activity;
     private ArrayList data;
-    private static LayoutInflater inflater = null;
     public Resources res;
-    ListModel tempValues = null;
-    int i = 0;
 
-    /*************  CustomAdapter Constructor *****************/
-    public CustomAdapter(Activity a, ArrayList d,Resources resLocal) {
+    /**
+     * **********  CustomAdapter Constructor ****************
+     */
+    public CustomAdapter(Activity a, ArrayList d, Resources resLocal) {
 
         /********** Take passed values **********/
         activity = a;
         data = d;
         res = resLocal;
-
-        /***********  Layout inflator to call external xml layout () **********************/
-        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
     }
 
-    /******** What is the size of Passed Arraylist Size ************/
+    @Override
     public int getCount() {
 
-        if(data.size()<=0)
+        if (data.size() <= 0)
             return 1;
         return data.size();
     }
 
+    @Override
     public Object getItem(int position) {
         return position;
     }
 
+    @Override
     public long getItemId(int position) {
         return position;
     }
 
-    /********* Create a holder to contain inflated xml file elements ***********/
-    public static class ViewHolder{
-
-        public Spinner text;
-        public Spinner text1;
-        public TextView player;
-
-    }
-
-    /*********** Depends upon data size called for each row , Create each ListView row ***********/
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        View vi=convertView;
-        ViewHolder holder;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_player, parent, false);
 
-        if (convertView == null){
+        }
 
-            /********** Inflate tabitem.xml file for each row ( Defined below ) ************/
-            vi = inflater.inflate(R.layout.tabitem, null);
+        final Spinner spiColor = (Spinner) convertView.findViewById(R.id.color);
+        spiColor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.w("Color", "" + spiColor.getSelectedItem());
+            }
 
-            /******** View Holder Object to contain tabitem.xml file elements ************/
-            holder=new ViewHolder();
-            holder.text = (Spinner) vi.findViewById(R.id.player);
-            holder.text1=(Spinner)vi.findViewById(R.id.color);
-            holder.player=(TextView)vi.findViewById(R.id.text);
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-            /************  Set holder with LayoutInflater ************/
-            vi.setTag(holder);
-        } else
-            holder=(ViewHolder)vi.getTag();
-            /***** Get each Model object from Arraylist ********/
-            tempValues = null;
-            tempValues = (ListModel) data.get(position);
+            }
+        });
 
-            /************  Set Model values in Holder elements ***********/
-            holder.player.setText("Joueur :");
-            /******** Set Item Click Listner for LayoutInflater for each row ***********/
-            vi.setOnClickListener(new OnItemClickListener(position));
-        return vi;
+        final Spinner spiPlayer = (Spinner) convertView.findViewById(R.id.player);
+        spiPlayer.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.w("Player", "" + spiPlayer.getSelectedItem());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        convertView.setOnClickListener(new OnItemClickListener(position));
+
+        return convertView;
     }
 
     @Override
@@ -104,18 +102,19 @@ public class CustomAdapter extends BaseAdapter   implements OnClickListener {
         Log.v("CustomAdapter", "=====Row button clicked");
     }
 
-    /********* Called when Item click in ListView ************/
-    private class OnItemClickListener  implements OnClickListener{
+    /**
+     * ****** Called when Item click in ListView ***********
+     */
+    private class OnItemClickListener implements OnClickListener {
         private int mPosition;
 
-        OnItemClickListener(int position){
+        OnItemClickListener(int position) {
             mPosition = position;
         }
 
         @Override
         public void onClick(View arg0) {
-            GameActivity sct = (GameActivity)activity;
-            sct.onItemClick(mPosition);
+
         }
     }
 }
