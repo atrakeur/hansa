e8f6090b83e1a8872674a43fdac4fec1439356a4
hayette <hayette.tahechent@etu.univ-rouen.fr>
2015-04-12 13:22:33 +0200

    feature(UI) add background image

--- app/src/main/java/fr/univ_rouen/hansa/activity/MainMenuActivity.java
+++ app/src/main/java/fr/univ_rouen/hansa/activity/MainMenuActivity.java
@@ -1,10 +1,8 @@
 package fr.univ_rouen.hansa.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import fr.univ_rouen.hansa.R;
 
@@ -17,22 +15,23 @@ public class MainMenuActivity extends Activity {
         setContentView(R.layout.activity_main_menu);
         createController();
     }
-    private void createController(){
+
+    private void createController() {
 
         //button new game
-        findViewById(R.id.new_game).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.new_game).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 //exemple comment appeler une autre activity
-                Intent intent = new Intent(MainMenuActivity.this, GameActivity.class);
+                /*Intent intent = new Intent(MainMenuActivity.this, GameActivity.class);
                 Toast.makeText(getApplicationContext(), "Chargement en cours...", Toast.LENGTH_SHORT).show();
-                startActivity(intent);
+                startActivity(intent);*/
             }
         });
 
         //button load game
-        findViewById(R.id.load_game).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.load_game).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -44,7 +43,7 @@ public class MainMenuActivity extends Activity {
         });
 
         //button options
-        findViewById(R.id.options_game).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.options_game).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -53,7 +52,7 @@ public class MainMenuActivity extends Activity {
         });
 
         //button exit
-        findViewById(R.id.exit_game).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.exit_game).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
