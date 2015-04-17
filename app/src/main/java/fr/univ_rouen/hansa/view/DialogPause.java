2fe3cf767347b8a0abc13c053bdb19c74fc48ac5
Valulz <vdesportes@gmail.com>
2015-04-17 19:15:51 +0200

    feat(UI) : Add to pause menu the possibility to go back to MainMenuActivity

--- app/src/main/java/fr/univ_rouen/hansa/view/DialogPause.java
+++ app/src/main/java/fr/univ_rouen/hansa/view/DialogPause.java
@@ -2,17 +2,22 @@ package fr.univ_rouen.hansa.view;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 
 import fr.univ_rouen.hansa.R;
+import fr.univ_rouen.hansa.activity.MainMenuActivity;
 
 public class DialogPause extends Dialog implements View.OnClickListener {
 
+    private Context context;
 
     public DialogPause(Context context) {
         super(context);
+        this.context = context;
         setContentView(R.layout.dialog_pause);
         setTitle(R.string.pause_title);
+
         findViewById(R.id.b_continue).setOnClickListener(this);
         findViewById(R.id.b_save).setOnClickListener(this);
         findViewById(R.id.b_rules).setOnClickListener(this);
@@ -36,7 +41,11 @@ public class DialogPause extends Dialog implements View.OnClickListener {
                 break;
             }
             case R.id.b_quit: {
-                //TODO intent to MainMenu
+                this.dismiss();
+
+                Intent intent = new Intent(context, MainMenuActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                context.startActivity(intent);
                 break;
             }
         }
