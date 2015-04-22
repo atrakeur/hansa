d2bdc3aa8cb77ab515151c36465b3d9990bea39c
Aku76 <steeven.cauchois@gmail.com>
2015-04-22 19:00:26 +0200

    refactor (VictoryCoellen) : all controls on additionnal points from Coellen are handle in VictoryCoellen (getter setter Merchant + getter Values)

---
+++ app/src/androidTest/java/fr/univ_rouen/hansa/gameboard/cities/VictoryCoellenTest.java
@@ -0,0 +1,32 @@
+package fr.univ_rouen.hansa.gameboard.cities;
+
+import junit.framework.TestCase;
+
+import fr.univ_rouen.hansa.gameboard.Privillegium;
+import fr.univ_rouen.hansa.gameboard.player.HTComputer;
+import fr.univ_rouen.hansa.gameboard.player.HTPlayer;
+import fr.univ_rouen.hansa.gameboard.player.PlayerColor;
+import fr.univ_rouen.hansa.gameboard.player.pawns.Merchant;
+
+public class VictoryCoellenTest extends TestCase {
+
+    public void testSetMerchantAndGetPawn() throws Exception {
+        //getPawn return null on empty
+        assertEquals(null,VictoryCoellen.getInstance().getPawn(Privillegium.White));
+
+        //getPawn return the previously setted pawn if not empty
+        Merchant m = new Merchant(new HTPlayer(PlayerColor.purple,1));
+        VictoryCoellen.getInstance().setMerchant(m,Privillegium.White);
+        assertEquals(m,VictoryCoellen.getInstance().getPawn(Privillegium.White));
+    }
+
+    public void testIsAvailable() throws Exception {
+        //test if everything available at the beginning
+        for(Privillegium p : Privillegium.values()){
+            assertEquals(true, VictoryCoellen.getInstance().isAvailable(p));
+        }
+        VictoryCoellen.getInstance().setMerchant(new Merchant(new HTPlayer(PlayerColor.purple,1)),Privillegium.White);
+        assertEquals(false, VictoryCoellen.getInstance().isAvailable(Privillegium.White));
+
+    }
+}
