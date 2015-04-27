922348e89527b2586f65f7116b595295532056b7
atrakeur <atrakeur@gmail.com>
2015-04-27 18:19:13 +0200

    fix (AI) Fixed AI playing time

--- app/src/main/java/fr/univ_rouen/hansa/gameboard/player/HTComputer.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/player/HTComputer.java
@@ -21,51 +21,7 @@ public class HTComputer extends HTPlayer {
         this.strategy.setPlayer(this);
     }
 
-    @Override
-    public void newTurn() {
-        super.newTurn();
-
-        run = true;
-        runThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                final String orgName = Thread.currentThread().getName();
-                Thread.currentThread().setName(orgName + " - AI " + getPlayerColor().name());
-                while (run) {
-                    //Wait for it
-                    do {
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    } while(TurnManager.getInstance().getCurrentPlayingPlayer() != HTComputer.this);
-
-                    //Get movements from strategy
-                    IMovement[] movements = strategy.compute(GameBoardFactory.getGameBoard());
-                    //Use movements (sould be safe cause we are awesome!)
-
-                    for (int i = 0; i < movements.length; i++) {
-                        MovementManager.getInstance().doMove(movements[i]);
-                    }
-
-                    if (TurnManager.getInstance().isNextTurnAvailable() == TurnManager.nextTurnRequire.bonusMarkers) {
-                        //Got bonus markers to replace
-                        //TODO
-                    }
-                    if (TurnManager.getInstance().isNextTurnAvailable() == TurnManager.nextTurnRequire.none) {
-                        //Got no more actiones? ready to move your fucking ass out of the way
-                        run = false;
-                        GameActivity.getInstance().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                TurnManager.getInstance().nextPlayer(true);
-                            }
-                        });
-                    }
-                }
-            }
-        });
-        runThread.start();
+    public ComputerStrategy getStrategy() {
+        return strategy;
     }
 }
