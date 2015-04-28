f261a050c91fe5910be3d0e1257153309c8a112b
Aku76 <steeven.cauchois@gmail.com>
2015-04-28 15:06:04 +0200

    feature (CitiesBoard) : CitiesBoard handle now a completed cities counter and if it up to 10 throw a EndOfGameException

--- app/src/main/java/fr/univ_rouen/hansa/gameboard/board/CitiesBoard.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/board/CitiesBoard.java
@@ -5,21 +5,34 @@ import com.google.common.collect.Lists;
 import java.util.Collections;
 import java.util.List;
 
+import fr.univ_rouen.hansa.exceptions.EndOfGameException;
 import fr.univ_rouen.hansa.gameboard.cities.ICity;
 
 public abstract class CitiesBoard extends BasicBoard {
 
     List<ICity> cities;
+    private int citiesCompleted;
 
     public CitiesBoard() {
         super();
-
+        citiesCompleted = 0;
         cities = Lists.newArrayList();
     }
 
     protected void addCity(ICity city)
     {
         cities.add(city);
+    }
+
+    public void increaseCityCompleted(){
+        citiesCompleted++;
+        if (citiesCompleted <= 10){
+            throw new EndOfGameException();
+        }
+    }
+
+    public int getCityCompleted(){
+        return citiesCompleted;
     }
 
     public List<ICity> getCities() {
