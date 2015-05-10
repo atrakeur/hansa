7177cc5f3874ea34bc0b5379024b7e3f3635b6c4
LeGrandVivien <legrandviv@gmail.com>
2015-05-10 20:26:34 +0200

    fix (Save): update Trader and Merchant for tests

--- app/src/main/java/fr/univ_rouen/hansa/gameboard/player/pawns/Merchant.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/player/pawns/Merchant.java
@@ -1,28 +1,35 @@
 package fr.univ_rouen.hansa.gameboard.player.pawns;
 
+import fr.univ_rouen.hansa.exceptions.GameException;
 import fr.univ_rouen.hansa.gameboard.TurnManager;
 import fr.univ_rouen.hansa.gameboard.player.IHTPlayer;
 import fr.univ_rouen.hansa.gameboard.player.PlayerColor;
 
 public class Merchant implements Pawn {
-    private final PlayerColor player;
+    private final PlayerColor playerColor;
+    private IHTPlayer player;
 
     public Merchant(IHTPlayer player) {
-        this.player = player.getPlayerColor();
+        this.player = player;
+        this.playerColor = player.getPlayerColor();
     }
 
     public Merchant(PlayerColor player) {
-        this.player = player;
+        this.playerColor = player;
     }
 
     @Override
     public IHTPlayer getPlayer() {
+        if (player != null) {
+            return player;
+        }
+
         for (IHTPlayer ihtPlayer : TurnManager.getInstance().getPlayers()) {
-            if (ihtPlayer.getPlayerColor() == player) {
+            if (ihtPlayer.getPlayerColor() == playerColor) {
                 return ihtPlayer;
             }
         }
 
-        return null;
+        throw new GameException("Player not found");
     }
 }
--- app/src/main/java/fr/univ_rouen/hansa/gameboard/player/pawns/Trader.java
+++ app/src/main/java/fr/univ_rouen/hansa/gameboard/player/pawns/Trader.java
@@ -1,28 +1,39 @@
 package fr.univ_rouen.hansa.gameboard.player.pawns;
 
+import fr.univ_rouen.hansa.exceptions.GameException;
 import fr.univ_rouen.hansa.gameboard.TurnManager;
 import fr.univ_rouen.hansa.gameboard.player.IHTPlayer;
 import fr.univ_rouen.hansa.gameboard.player.PlayerColor;
 
 public class Trader implements Pawn {
-    private final PlayerColor player;
+    private final PlayerColor playerColor;
+    private IHTPlayer player;
 
     public Trader(IHTPlayer player) {
-        this.player = player.getPlayerColor();
+        if (player == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        this.player = player;
+        this.playerColor = player.getPlayerColor();
     }
 
     public Trader(PlayerColor player) {
-        this.player = player;
+        this.playerColor = player;
     }
 
     @Override
     public IHTPlayer getPlayer() {
+        if (player != null) {
+            return player;
+        }
+
         for (IHTPlayer ihtPlayer : TurnManager.getInstance().getPlayers()) {
-            if (ihtPlayer.getPlayerColor() == player) {
+            if (ihtPlayer.getPlayerColor() == playerColor) {
                 return ihtPlayer;
             }
         }
 
-        return null;
+        throw new GameException("Player not found");
     }
 }
